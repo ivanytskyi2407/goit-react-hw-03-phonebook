c7f616ad92928e70d7ccb08503096508cfa8087e
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-05-08 21:39:58 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,19 @@ export class App extends Component {
     ],
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     const noUniqueName = this.state.contacts
